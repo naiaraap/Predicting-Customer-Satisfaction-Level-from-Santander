b8233c8aa651b6ca72f5878fb352645fd72ac34e
naiaraap <naiarapantuza@gmail.com>
2020-03-10 19:10:17 -0300

    src

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+# This script contains useful functions for data processing
+# Importing libraries and frameworks
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats
